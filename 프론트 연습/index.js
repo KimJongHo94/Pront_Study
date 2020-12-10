fab952f6abc5cbad0f9499e60a1361d3e07b873d
Nick65 <55225790+KimJongHo94@users.noreply.github.com>
2020-12-11 00:00:15 +0900

    Add files via upload

---
+++ 프론트 연습/index.js
@@ -0,0 +1,10 @@
+// console.log(3000 * 2);
+// console.log(4200 * 4 + 4800 * 2);
+// console.log(4200 + 4800);
+// console.log(3000 + 4200 * 4 + 4800 * 2);
+// console.log(4200 * 3);
+
+// 변수 선언
+let espressPrice = 3000;
+
+console.log(espressPrice);
